35e36ba4c2995f1ab4911c131b51f525d974e667
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-03-14 21:51:35 +0700

    menambahkan fungsi pengumuman

---
+++ application/models/pengumuman_model.php
@@ -0,0 +1,16 @@
+<?php
+class pengumuman_model extends CI_Model{
+	public function __construct() {
+	$this->load->database();
+	}
+	   
+	   // Menampilkand data pengumuman
+		public function daftar_pengumuman($read = FALSE) {
+		if ($read === FALSE) {
+		$query = $this->db->query('SELECT * FROM pengumuman WHERE status_pengumuman ="Publish" ORDER BY id_pengumuman DESC');
+		return $query->result_array();
+		}
+		$query = $this->db->get_where('pengumuman', array('slug' => $read));
+		return $query->row_array();
+		}
+}
