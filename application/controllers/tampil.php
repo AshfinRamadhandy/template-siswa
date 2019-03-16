4a748d4d3a0a1ba39ffe9c65adef278b73b19550
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-03-16 16:47:59 +0700

    tampil data proses

---
+++ application/controllers/tampil.php
@@ -0,0 +1,14 @@
+<?php
+class tampil extends CI_Controller{
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+                $this->load->helper('url');
+	}
+ 
+	function index(){
+		$data['data_siswa'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_tampil',$data);
+	}
+
+}
