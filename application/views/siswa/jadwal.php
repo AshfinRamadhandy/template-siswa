41ab20a3d1d02bc175b0655b7e0d95e47804adc9
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-04-02 14:35:15 +0700

    download jadwal

--- application/views/siswa/jadwal.php
+++ application/views/siswa/jadwal.php
@@ -3,10 +3,11 @@
 <div class="jumbotron text-center"> 
       <h3> Lihat Jadwal </h3>
       <div class="panel panel-default">
-        <div class="panel-heading">
+        <!-- <div class="panel-heading">
             <i class="fa fa-external-link-square">Download Jadwal</i> 
-        </div>
+        </div> -->
         <div class="panel-body">
+				<a href="<?php echo base_url().'index.php/downloadjadwal/lakukan_download' ?>">Download File</a>
         <button type="edit" name="edit" class="btn btn-primary" >Download Jadwal</button>
 </section> 
 </div>
