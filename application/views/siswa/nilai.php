6d674aedc1cbbd3ff95b74d5b7e3ccf9455084a5
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-04-04 22:51:08 +0700

    tampilkan nilai

--- application/views/siswa/nilai.php
+++ application/views/siswa/nilai.php
@@ -1,7 +1,9 @@
 <div class="container">
 <section class="content">
+<div class="jumbotron text-center"> 
+      <h3> Lihat Nilai </h3>
     <div class="inner">
-    <button type="button" class="btn btn-primary"></a>Lihat Nilai</button>
+    <button type="button" class="btn btn-primary"></a>Tampilkan Nilai</button>
         </div>
 </section>
 </div>
